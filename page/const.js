b757ad76e219424e19146d264a1f7e879221455c
vihashah <viha.shah@sjsu.edu>
2023-10-28 22:07:52 -0700

    :sparkles: both work!

---
+++ page/const.js
@@ -0,0 +1,1 @@
+export const url = 'https://us-east-1.aws.data.mongodb-api.com/app/application-1-vghrw/endpoint'
--- page/index.js
+++ page/index.js
@@ -1,6 +1,10 @@
 import * as hmUI from "@zos/ui";
 import { log as Logger } from "@zos/utils";
 import { BasePage } from "@zeppos/zml/base-page";
+import { url } from "./const";
+
+// import { layout } from 'zosLoader:./index.[pf].layout.js'
+import { Geolocation } from "@zos/sensor";
 import {
   PUT_BUTTON,
   GET_BUTTON,
@@ -8,17 +12,35 @@ import {
 } from "zosLoader:./index.[pf].layout.js";
 
 const logger = Logger.getLogger("fetch_api");
+const geolocation = new Geolocation();
 
 let textWidget;
 Page(
   BasePage({
     state: {},
+    onInit() {
+      const callback = () => {
+        console.log('pos_status', geolocation.getStatus())
+
+        const lat = geolocation.getLatitude();
+        const long = geolocation.getLongitude();
+        this.syncData({
+          lat,
+          long
+        })
+      }
+      geolocation.start();
+      geolocation.onChange(callback);
+
+      logger.log('page onInit invoked');
+    },
     build() {
+
       hmUI.createWidget(hmUI.widget.BUTTON, {
         ...PUT_BUTTON,
         click_func: (button_widget) => {
           logger.log("click button");
-          this.postData();
+          // this.postData();
         },
       });
       hmUI.createWidget(hmUI.widget.BUTTON, {
@@ -28,6 +50,7 @@ Page(
           this.fetchData();
         },
       });
+      // layout.render(this);
     },
     fetchData() {
       this.request({
@@ -48,6 +71,29 @@ Page(
         })
         .catch((res) => {});
     },
+
+
+    syncData(data) {
+      // layout.updateTxtUploading()
+      console.log("changing data");
+      return this.httpRequest({
+        method: 'post',
+        url: `${url}/locationData`,
+        body: data,
+      })
+      .then((result) => {
+        // layout.updateTxtSuccess()
+        logger.log('result => %j', result)
+      })
+      .catch((error) => {
+        // layout.updateTxtError()
+        logger.error('error => %j', error);
+      })
+    },
+    onDestroy() {
+      logger.log('page onDestroy invoked')
+    }
+
     // postData() {
     //   this.request({
     //     method: "GET_DATA"
